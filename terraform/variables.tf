443cd310300ce4490ca5c869bb4aed1a70b99407
Hydervalli Shaik <hihyder123@gail.com>
2025-07-06 16:55:09 +0530

    updated

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,6 +7,11 @@ variable "ami" {
   default = "ami-0c02fb55956c7d316" # Amazon Linux 2
 }
 
+variable "key_name" {
+  description = "EC2 Key pair name for SSH access"
+  default     = "my-keypair-name"
+}
+
 variable "instance_type" {
   default = "t2.micro"
 }
